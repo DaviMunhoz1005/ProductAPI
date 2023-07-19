be39f65aca20178f4e571b631ee698d7199134d6
Davi Munhoz <davimunhoz1005@gmail.com>
2023-07-18 21:12:44 -0300

    Created request API start, about and contact

--- src/main/java/com/api/product/controller/ProductController.java
+++ src/main/java/com/api/product/controller/ProductController.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     //Request Start API
-    @GetMapping(path = "/start")
+    @GetMapping(path = "start")
     public @ResponseBody String start() {
-        return "Welcome to products API";
+        return "Welcome to products API!";
     }
 
     //Request About API
-    @GetMapping(path = "/about")
+    @GetMapping(path = "about")
     public @ResponseBody String about() {
-        return "About the API";
+        return "About the API...";
     }
 
     //Request Contact API
-    @GetMapping(path = "/contact")
+    @GetMapping(path = "contact")
     public @ResponseBody String contact() {
         return "Contact us :)";
     }
